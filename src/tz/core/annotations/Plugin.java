c9df7bda9de97e01db0bde3b6c0492d9afe4a477
= <terrahen@web.de>
2015-05-10 20:35:15 +0200

    data

--- src/tz/core/annotations/Plugin.java
+++ src/tz/core/annotations/Plugin.java
@@ -1,5 +1,10 @@
 package tz.core.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 
  * @author terrazero
@@ -10,6 +15,8 @@ package tz.core.annotations;
  * @identifier tz.core.annotations
  *
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Plugin {
 
 	public String name();
